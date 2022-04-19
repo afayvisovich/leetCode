569cc43759794a93069e455a24c68015ca793368
Andrey Fayvisovich <andrey.fayvisovich@raiffeisen.ru>
2022-04-20 00:26:14 +0300

    fix: Refactoring

--- CommonPrefix.js
+++ CommonPrefix.js
@@ -4,25 +4,21 @@
  */
 
  const longestCommonPrefix = (strs) => {
-    if (strs[0] === "") {
-        return ""
-    }
     let prefix = ""
     while (prefix.length < strs[0].length) {
-        const symbol = strs[0][prefix.length]
         for (j = 1; j < strs.length; j++) {
             if (prefix.length > strs[j].length) {
                 return prefix
             }
-            if (symbol != strs[j][prefix.length]) {
+            if (strs[0][prefix.length] != strs[j][prefix.length]) {
                 return prefix
             }
         }
-        prefix += symbol
+        prefix += strs[0][prefix.length]
     }
     return prefix
 };
 
 
 
-console.log(longestCommonPrefix(["flower","flow","flight"]))
+console.log(longestCommonPrefix([""]))
